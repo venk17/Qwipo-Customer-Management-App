86fb0777e89f8110193cc8892182e567e4128076
venk17 <k.venkatramanan2002@gamil.com>
2025-08-30 20:17:35 +0530

    update backend url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = '/api';
+const API_BASE_URL = 'https://qwipo-customer-management-app.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
